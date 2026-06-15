e3621da7bb4d0b57297d01974901356f619fec9a
magostores38-commits <magostores38@gmail.com>
2026-06-15 14:24:54 -0300

    Update Atualicacoes.lua

--- Atualicacoes.lua
+++ Atualicacoes.lua
@@ -1,8 +1,8 @@
-• [NOVO] Copiar Roupa 100% (Clona skin e trajes na hora)
-• [NOVO] Armário Completo: Modifique Roupas, Tatuagens e Cabelo
-• [NOVO] Fixar Carro no Chão & Freio Instantâneo
-• [NOVO] Suicídio Nativo + Sistemas de Curar, Reviver e Novo UNLOCK
-• [UPDATE] NoClip Otimizado (Mais liso) + Opção de Invisível + esp name ok
-• [VEÍCULOS] MaxTuning, Super Boost Infinito, Desvirar, Reparar e Trocar Placa
-• [VISUAIS] ESP atualizado com novos filtros e funções
-• [COMBATE] Fast Reload ajustado 
+[NOVO] Copiar Roupa 100% (Clona skin e trajes na hora)
+[NOVO] Armário Completo: Modifique Roupas, Tatuagens e Cabelo
+[NOVO] Fixar Carro no Chão & Freio Instantâneo
+[NOVO] Suicídio Nativo + Sistemas de Curar, Reviver e Novo UNLOCK
+[UPDATE] NoClip Otimizado (Mais liso) + Opção de Invisível + esp name ok
+[VEÍCULOS] MaxTuning, Super Boost Infinito, Desvirar, Reparar e Trocar Placa
+[VISUAIS] ESP atualizado com novos filtros e funções
+[COMBATE] Fast Reload ajustado 
